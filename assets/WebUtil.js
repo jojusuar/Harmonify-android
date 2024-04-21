9ace87ec3687f51a1fbec63618a717c2a8e40fcb
José Julio Suárez <jojusuar@espol.edu.ec>
2024-04-21 18:23:18 -0500

    Delete assets directory

--- assets/WebUtil.js
+++
@@ -1,6 +0,0 @@
-if (!sessionStorage.getItem('reloaded')) {
-    sessionStorage.setItem('reloaded', 'true');
-    window.location.reload(true);
-} else {
-    sessionStorage.removeItem('reloaded');
-}
